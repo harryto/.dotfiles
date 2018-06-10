46032e2ea4c783f757d7605c6e4b375aa7108d9c
Harry To <to000002@umn.edu>
2018-06-10 21:09:36 +0700

    cleaned up

---
+++ bin/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# install script for the dotfiles of Harry
+# this looks ok now but wait till it gets out of hand at some 
+# point in time 
+
+# make sure vundle exists before installing 
+if [ -e ~/.vim/bundle/Vundle.vim ];
+then 
+	echo "found Vundle, therefore won't install vundle a second time cause that's dumb; "
+else
+	echo "\n\nNo Vundle found, installing vundle; \n\n"
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	echo "\n\nNow running install on vim \\n" 
+	vim +PluginInstall +qall
+fi
+
+# I think this goes the same for our tmux file 
+if [ -f /usr/local/bin/tmux-vim-select-pane ];
+then 
+	echo "found tmux, not installing"
+else
+	echo "going to copy tmux, vim smooth screen transitioning" 
+	cp usrtmuxvim.sh /usr/local/bin/tmux-vim-select-pane
+	chmod +x /usr/local/bin/tmux-vim-select-pane
+fi
